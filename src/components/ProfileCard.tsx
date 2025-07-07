0881847e4b03a0b744b0fd126558493349a4af36
Meshach Arinze <arinzemeshach123@gmail.com>
2025-07-08 00:45:39 +0100

    feat: chnage the name of author and location

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -7,15 +7,15 @@ export default function ProfileCard() {
       <div className="w-54 h-40 mb-4">
         <SvgAvatar />
       </div>
-      <h2 className="font-bold text-lg mb-2">Greg Wientjes</h2>
+      <h2 className="font-bold text-lg mb-2">Meshach Arinze</h2>
       <div className="w-full text-sm">
         <div className="mb-2">
           <div className="font-semibold">Networks</div>
           <div>Stanford Alum</div>
         </div>
         <div>
-          <div className="font-semibold">Current City</div>
-          <div>Palo Alto, CA</div>
+          <div className="font-semibold">Current Location</div>
+          <div>Lagos Nigeria</div>
         </div>
       </div>
     </aside>
