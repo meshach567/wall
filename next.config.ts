844270936b1d7245cece2b1b6be19c29436e4384
Meshach Arinze <arinzemeshach123@gmail.com>
2025-07-08 16:08:42 +0100

    chore: include the domain naime for images inside next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: [
+      "bfttfsfxgnfkdbmfgnuj.supabase.co", // <-- add your Supabase domain here
+    ],
+  },
 };
 
 export default nextConfig;
